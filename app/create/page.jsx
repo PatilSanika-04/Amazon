ff60d89b30cc3dcaf917dfa532fc349875aa2ba8
PatilSanika-04 <patilsanika0412@gmail.com>
2025-06-18 01:40:55 +0530

    changes on css

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -30,17 +30,30 @@ export default function UserPage() {
 
  
   return (
+    
+      
+    // </div>
     <div className="p-6 max-w-md mx-auto">
-      <h1 className="text-xl font-bold mb-4">Add a New Product in your Bucket list: </h1>
-      
-
+      <h1 className="text-xl font-bold mb-4">Add a New Product to Your Bucket List:</h1>
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-        <input {...register('name')} placeholder="Name of product" className="w-full p-2 border rounded" />
-        <input {...register('price')} placeholder="price of product" className="w-full p-2 border rounded" />
-        <button type="submit" className="bg-black-500 text-black px-4 py-2 rounded">Submit</button>
+        <div>
+          <input
+            {...register('name')}
+            placeholder="Name of product"
+            className="w-full p-2 border rounded text-black placeholder-black focus:text-red-500 focus:outline-none"
+          />
+        </div>
+        <div>
+          <input
+            {...register('price')}
+            placeholder="Price of product"
+            className="w-full p-2 border rounded text-black placeholder-black focus:text-red-500 focus:outline-none"
+          />
+        </div>
+        <button type="submit" className="bg-black text-white px-4 py-2 rounded">
+          Submit
+        </button>
       </form>
-
-      
     </div>
   );
 }
